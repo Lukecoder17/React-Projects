8a2c8745c8b2035b615e4784662c955fdb0e1cbd
Lukecoder17 <lukebthomas17@outlook.com>
2021-12-10 14:27:58 +0000

    deploy my react app to github pages

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './brand.css';
+import {google, slack, atlassian, dropbox, shopify} from './imports';
+
+function Brand() {
+  return (
+     <div className="gpt3__brand section__padding">
+       <div className="">
+      <img src={google} alt="google" />
+       </div>
+       <div className="">
+      <img src={slack} alt="slack" />
+       </div>
+       <div className="">
+      <img src={atlassian} alt="atlassian" />
+       </div>
+       <div className="">
+      <img src={dropbox} alt="dropox" />
+       </div>
+       <div className="">
+      <img src={shopify} alt="shopify" />
+       </div>
+    </div>
+  )
+}
+
+export default Brand;
